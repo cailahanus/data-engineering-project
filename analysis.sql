a8e2549cbe2cdd6fb95a434498ae263480eda316
cailahanus <153878457+cailahanus@users.noreply.github.com>
2024-04-18 21:21:09 -0500

    Add files via upload

---
+++ analysis.sql
@@ -0,0 +1,35 @@
+-- SQL Queries to verify that csv info uploaded to tables 
+
+--checking Data table
+SELECT *
+FROM Data
+
+--checking Inidcators table
+SELECT *
+FROM Indicators
+
+--checking Regions table
+SELECT *
+FROM Regions
+
+-- If Regions Table was split
+
+--checking City table
+SELECT *
+FROM City
+
+--checking County table
+SELECT *
+FROM County
+
+--checking Metro table
+SELECT *
+FROM Metro
+
+--checking Neighborhood table
+SELECT *
+FROM Neighborhood
+
+--checking Zip table
+SELECT *
+FROM Zip
